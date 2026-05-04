458172775b550656545c93e613d31c1125be3778
Manish Kumar <manishrajkumar856@gmail.com>
2026-05-04 12:51:15 +0530

    User added

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,16 @@ app.get('/api/data',(req,res)=>{
     res.json(data);
 });
 
+app.get('/api/user/:id',(req,res)=>{
+    const userId=req.params.id;
+    const user={
+        id:userId,
+        name:'User '+userId,
+        email:'rahxerls.gmail.com'
+    };
+    res.json(user);
+}); 
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
