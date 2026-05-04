5fc3095e08cf37b5d326eaec8aaa2a8190152664
Manish Kumar <manishrajkumar856@gmail.com>
2026-05-04 12:45:55 +0530

    Add data route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ app.get('/',(req,res)=>{
     res.send('Hello World!');
 });
 
+app.get('/api/data',(req,res)=>{
+    const data={
+        message:'This is some data from the server',
+        timestamp:new Date()
+    };
+    res.json(data);
+});
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
